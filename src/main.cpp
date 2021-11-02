754e72123797bffd6d706bdb6e1e99103f86d06a
unknown <ht19a019@oecu.jp>
2021-11-02 13:42:44 +0900

    111

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
+#include <iostream> 
+#include <vector>
+
+#define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。 
+
 bool IsPrime(int num)
 {
-    if (num < 2) return false;
-    else if (num == 2) return true;
-    else if (num % 2 == 0) return false; 
+	if (num < 2) return false;
+	else if (num == 2) return true;
+	else if (num % 2 == 0) return false; 
 
-    double sqrtNum = sqrt(num);
-    for (int i = 3; i <= sqrtNum; i += 2)
-    {
-        if (num % i == 0)
-        {
-            return false;
-        }
-    }
+	double sqrtNum = sqrt(num);
+	for (int i = 3; i <= sqrtNum; i += 2)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
 
-    return true;
+	return true;
 }
 /* --------------------------------------------------------------- */
 /*
@@ -27,26 +32,33 @@ bool IsPrime(int num)
  */
  /* -------------------------------------------------------------- */
 int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
-    unsigned int* numptr;
-    numptr = (unsigned int *)malloc(sizeof(unsigned int) * CPP2_PRIME_UPPER_LIMIT);
+	unsigned int* numptr;
+	numptr = (unsigned int *)malloc(sizeof(unsigned int) * CPP2_PRIME_UPPER_LIMIT);
 
-    unsigned int* prime;
-    prime = (unsigned int *)malloc(sizeof(unsigned int) * CPP2_PRIME_UPPER_LIMIT);
-    //最初の数字
-    numptr[0] = a;
+	unsigned int* prime;
+	prime = (unsigned int *)malloc(sizeof(unsigned int) * CPP2_PRIME_UPPER_LIMIT);
+	//最初の数字
+	numptr[0] = a;
 
-    for (int i = 1; i < CPP2_PRIME_UPPER_LIMIT; i++) {
-        numptr[i] = numptr[i - 1] + d;
-    }
-    
-    uint32_t prime_index = 0;
-    for (int i = 0; i < CPP2_PRIME_UPPER_LIMIT; i++) {
-        if (IsPrime(numptr[i]) == true) {
-            prime[prime_index] = numptr[i];
-            prime_index++;
-        }
-    }
+	for (int i = 1; i < CPP2_PRIME_UPPER_LIMIT; i++) {
+		numptr[i] = numptr[i - 1] + d;
+	}
+	
+	uint32_t prime_index = 0;
+	for (int i = 0; i < CPP2_PRIME_UPPER_LIMIT; i++) {
+		if (IsPrime(numptr[i]) == true) {
+			prime[prime_index] = numptr[i];
+			prime_index++;
+		}
+	}
 
 
-    return prime[n - 1];
+	return prime[n - 1];
 };
+
+
+int main() {
+	std::cout << nth_prime(367,186,151) << std::endl;
+
+	return 0;
+}
