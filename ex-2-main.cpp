33e2e059b8d40b3db8331d8fef29dd6cf486244a
unknown <ht19a019@oecu.jp>
2021-12-14 17:59:46 +0900

    unit

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -1,80 +1,97 @@
-1.	#include <iostream>
-2.	#include <vector>
-3.	#include <cerrno>
-4.
-5.	#define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。 
-6.
-7.	bool IsPrime(int num)
-8.	{
-	9.		if (num < 2) return false;
-	10.		else if (num == 2) return true;
-	11.		else if (num % 2 == 0) return false;
-	12.
-		13.		double sqrtNum = sqrt(num);
-	14.		for (int i = 3; i <= sqrtNum; i += 2)
-		15.		{
-		16.			if (num % i == 0)
-			17.			{
-			18.				return false;
-			19.			}
-		20.		}
-	21.
-		22.		return true;
-	23.	}
-24.	/* --------------------------------------------------------------- */
-25.	/*
-	26.	 *  nth_prime
-	27.	 *
-	28.	 *  与えられた正整数 a と d と n に対して、この等差数列に含まれる n 番目の
-	29.	 *  素数を返す。
-	30.	 *
-	31.	 *  TODO: この nth_prime 関数を実装せよ。必要であれば他に関数や
-	32.	 *  ファイルを作成せよ。
-	33.	 */
-	34.	 /* -------------------------------------------------------------- */
-	35.	int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
-	36.		if (n <= 0 || (a <= 0 || d <= 0)) {
-		37.			std::cerr << "invalid arguments";
-		38.			exit(EXIT_FAILURE);
-		39.
-	}
-	40.
-		41.		uint32_t* prime;
-	42.		prime = (uint32_t *)malloc(sizeof(uint32_t) * n);
-	43.
-		44.		uint32_t prime_index = 0;
-	45.		uint32_t num = a;
-	46.
-		47.		for (int i = 1; i < CPP2_PRIME_UPPER_LIMIT; i++) {
-		48.			if (IsPrime(num) == true) {
-			49.				prime[prime_index] = num;
-			50.				prime_index++;
-			51.				if (prime_index > n) {
-				52.					break;
-				53.
+#include <iostream>
+#include <string>
+namespace cpp2 {
+	/* --------------------------------------------------------------------- */
+	/*
+	mcxi
+	mcxi 記法を解析するクラスです。
+	*/
+	/* --------------------------------------------------------------------- */
+	class mcxi {
+	private:
+		int value_;
+		int unit(char c) {
+			switch (c) {
+			case 'm': return 1000;
+			case 'c': return 100;
+			case 'x': return 10;
+			case 'i': return 1;
 			}
-			54.
 		}
-		55.			num += d;
-		56.
-	}
-	57.		return prime[n - 1];
-	58.
-};
-59.
-60.	int main() {
-	61.		std::cout << nth_prime(367, 186, 151) << std::endl;
-	62.		std::cout << nth_prime(179, 10, 203) << std::endl;
-	63.		std::cout << nth_prime(271, 37, 39) << std::endl;
-	64.		std::cout << nth_prime(103, 230, 1) << std::endl;
-	65.		std::cout << nth_prime(27, 104, 185) << std::endl;
-	66.		std::cout << nth_prime(253, 50, 85) << std::endl;
-	67.		std::cout << nth_prime(1, 1, 1) << std::endl;
-	68.		std::cout << nth_prime(9075, 337, 210) << std::endl;
-	69.		std::cout << nth_prime(307, 24, 79) << std::endl;
-	70.		std::cout << nth_prime(331, 221, 177) << std::endl;
-	71.		std::cout << nth_prime(259, 170, 40) << std::endl;
-	72.		std::cout << nth_prime(269, 58, 102) << std::endl;
-	73.
-		74.		return 0;
+	public:
+		char c[4] = { 'm', 'c', 'x', 'i' };
+		int v[4] = { 1000, 100, 10, 1 };
+		mcxi operator + (mcxi rhs)
+		{
+			mcxi nm("");
+			nm.value_ = this->value_ + rhs.value_;
+			return nm;
+		}
+		mcxi::mcxi(const std::string& s) : value_(0) {
+			int cc = 0;
+			int cs = 0;
+			int d = 1;
+			for (int i = 0; i < s.size(); i++) {
+				for (int j = 0; j < 4; j++) {
+					if (s[i] == c[j]) {
+						cc = j;
+						value_ += d*v[cc];
+						d = 1;
+					}
+					else { cs++; }
+					if (cs == 4) {
+						d = (int)(s[i] - '0');
+						cs = 0;
+					}
+				}
+				cs = 0;
+			}
+		}
+		std::string mcxi::to_string()
+		{
+			return std::to_string(value_);
+		}
+	};
+} // namespace cpp2
+int main() {
+	cpp2::mcxi a0("xi");
+	cpp2::mcxi b0("x9i");
+	cpp2::mcxi result0 = a0 + b0;
+	std::cout << "3x" << " " << result0.to_string() << std::endl;
+	cpp2::mcxi a1("i");
+	cpp2::mcxi b1("9i");
+	cpp2::mcxi result1 = a1 + b1;
+	std::cout << "x" << " " << result1.to_string() << std::endl;
+	cpp2::mcxi a2("c2x2i");
+	cpp2::mcxi b2("4c8x8i");
+	cpp2::mcxi result2 = a2 + b2;
+	std::cout << "6cx" << " " << result2.to_string() << std::endl;
+	cpp2::mcxi a3("m2ci");
+	cpp2::mcxi b3("4m7c9x8i");
+	cpp2::mcxi result3 = a3 + b3;
+	std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
+	cpp2::mcxi a4("9c9x9i");
+	cpp2::mcxi b4("i");
+	cpp2::mcxi result4 = a4 + b4;
+	std::cout << "m" << " " << result4.to_string() << std::endl;
+	cpp2::mcxi a5("i");
+	cpp2::mcxi b5("9m9c9x8i");
+	cpp2::mcxi result5 = a5 + b5;
+	std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
+	cpp2::mcxi a6("m");
+	cpp2::mcxi b6("i");
+	cpp2::mcxi result6 = a6 + b6;
+	std::cout << "mi" << " " << result6.to_string() << std::endl;
+	cpp2::mcxi a7("i");
+	cpp2::mcxi b7("m");
+	cpp2::mcxi result7 = a7 + b7;
+	std::cout << "mi" << " " << result7.to_string() << std::endl;
+	cpp2::mcxi a8("m9i");
+	cpp2::mcxi b8("i");
+	cpp2::mcxi result8 = a8 + b8;
+	std::cout << "mx" << " " << result8.to_string() << std::endl;
+	cpp2::mcxi a9("9m8c7xi");
+	cpp2::mcxi b9("c2x8i");
+	cpp2::mcxi result9 = a9 + b9;
+	std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
 }# ht19a019-cpp2
